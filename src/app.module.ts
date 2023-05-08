065c722066c9566ad6961460da35a15cb884f8b1
Tan <>
2023-05-08 19:57:56 +0700

    add lesson entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: 'mongodb',
+        url: config.get('MONGO_URL'),
+        synchronize: true,
+        useUnifiedTopology: true,
+        entities: [Lesson],
+      }),
+      inject: [ConfigService],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
